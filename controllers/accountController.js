33b17fd36be00a3a8fe4bcf73b73709f666c874a
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-12 08:28:44 -0400

    Refactor accountController.js and add login and registration functionality

---
+++ controllers/accountController.js
@@ -0,0 +1,73 @@
+const utilities = require("../utilities/")
+const accountModel = require("../models/account-model")
+require("dotenv").config()
+
+/* ****************************************
+*  Deliver login view (get /login)
+* *************************************** */
+async function buildLogin(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/login", {
+    title: "Login",
+    nav,
+    errors: null,
+  })
+}
+
+/* ****************************************
+*  Deliver registration view
+* *************************************** */
+async function buildRegister(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null,
+  })
+}
+
+async function registerAccount(req, res) {
+  let nav = await utilities.getNav()
+  const { account_firstname, account_lastname, account_email, account_password, account_password_confirmation } = req.body
+
+  // Check if passwords match
+  if (account_password !== account_password_confirmation) {
+    req.flash("notice", "Passwords do not match")
+    return res.status(400).render("account/register", {
+      title: "Register",
+      nav,
+      errors: null,
+      account_firstname,
+      account_lastname,
+      account_email,
+    })
+  }
+
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+
+  if (regResult) {
+    req.flash(
+      "success",
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+    )
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+    })
+  } else {
+    req.flash("notice", "Sorry, the registration failed.")
+    res.status(501).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+}
+
+  module.exports = { buildLogin, buildRegister, registerAccount }
